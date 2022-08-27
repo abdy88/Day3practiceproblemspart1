ef527e4ad02ba007d25639fb7b444f5c2e1a5087
aytech11 <aytech0110@gmail.com>
2022-08-27 19:44:03 +0530

    UC3 Adding part time employee and wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -14,7 +14,7 @@ public class EmployeeWageComputation {
 	
 				checkattendanceUC1();
 				dailyemployeewageUC2();
-				
+				AddparttimeemployeeandwageUC3();
 	
 		}
 	
@@ -48,18 +48,67 @@ public class EmployeeWageComputation {
 							int empRatePerHr=20;
 							int empHrs=8;
 							int salary= empRatePerHr*empHrs;
-							System.out.println("Daily employee wage is =  " + salary);
+							System.out.println("Employee present hence Daily employee wage is =  " + salary);
 						}
 					
 					else
 						{
 							int salary=0;
-							System.out.println("Daily employee wage is =  " + salary);
+							System.out.println("Employee absent hence Daily employee wage is =  " + salary);
 						
 						
 						}
 					
 				}
-									
+			
+			    static void AddparttimeemployeeandwageUC3() {
+			    	
+			    	int salary=0;
+				
+					byte isPresent=1;
+					byte isPartTime=2;
+					byte isFulltime=3;
+					
+//					double empCheck = Math.floor(Math.random()  *10) % 2  ; 
+					
+					double partOrFulltime =  Math.floor(Math.random()  *4);
+					
+					System.out.println("Checking again for another day  if Part time or full time or absent ");
+					
+//					System.out.println(empCheck);
+//					System.out.println(partOrFulltime);
+					
+					
+					if (empCheck==isPresent && partOrFulltime==isFulltime)
+					{	
+						int empRatePerHr=20;
+						int empHrs=10;
+						salary= empRatePerHr*empHrs;
+						System.out.println("Employee worked Full time hence Daily employee wage is =  " + salary);
+					}
+					
+					else if (empCheck==isPresent && partOrFulltime==isPartTime)
+					{
+						
+						int empRatePerHr=20;
+						int empHrs=8;
+						salary= empRatePerHr*empHrs;
+						System.out.println("Employee worked Part time hence Daily employee wage is =  " + salary);
+						
+					}
+					
+					else {
+						salary=0;
+						System.out.println("Employee absent hence Daily employee wage is =  " + salary);
+					
+						
+					}
+					
+				}
+			
+			
+			
+			
+								
 
 }
