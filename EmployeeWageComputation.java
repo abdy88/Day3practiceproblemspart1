8bdcbe3c4b438ca4127aa246dd4903404a5469a9
aytech11 <aytech0110@gmail.com>
2022-08-27 18:23:03 +0530

    Employee wage Computation Program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,13 @@
 package day3PracticeProblemsPart1;
 
 public class EmployeeWageComputation {
+	
+	
+	public static void main(String[] args) {
+	
+	
+	System.out.println("Welcome to Employee Wage Computation Program");
+	
+	}
 
 }
